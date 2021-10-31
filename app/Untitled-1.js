8c827aac83e46edf8a166740c2d3d4e747ce5f3d
ash09er5 <ash09er5@gmail.com>
2021-10-31 17:02:58 -0400

    Add files via upload

---
+++ app/Untitled-1.js
@@ -0,0 +1,17 @@
+// Original addition function
+function addition(a, b) {
+    return a + b;
+  }
+
+  // Converted to an arrow function
+addition = (a, b) => a + b;
+Once again, a multi-line function 
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+}
+
+doubleAddition = (c, d)doubleAddition = (c, d)
+=> addition(c, d) * 2;
